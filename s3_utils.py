fb74d275d6b8ca9de7277685216832a0b0bdd738
ellylai <ellysel@andrew.cmu.edu>
2025-10-26 13:20:26 -0400

    feat: added generic download and upload util functions for files and folder

--- s3_utils.py
+++ s3_utils.py
@@ -36,6 +36,58 @@ def upload_json_file(local_file_path, s3_object_name):
         return False
 
 
+def upload_folder(local_folder_path, s3_folder_name, file_type="octet-stream"):
+    """
+    Upload a folder to S3 bucket
+
+    Args:
+        local_folder_path (str): The local object name/path
+        s3_folder_name (str): The S3 object name/path
+        file_type (str): the file type
+    """
+    try:
+        # Upload each of the files in the folder
+        for filename in os.listdir(local_folder_path):
+            local_path = os.path.join(local_folder_path, filename)
+            if os.path.isdir(local_path):  # only upload files in the folder
+                continue
+            s3_object_name = f"{s3_folder_name}/{filename}"
+            client.fput_object(
+                bucket_name,
+                s3_object_name,
+                local_path,
+                content_type=f"application/{file_type}",
+            )
+        print(f"Successfully uploaded {local_folder_path} as {s3_folder_name}")
+        return True
+    except Exception as e:
+        print(f"Error uploading {local_folder_path}: {e}")
+        return False
+
+
+def upload_generic_file(local_file_path, s3_object_name, file_type="octet-stream"):
+    """Upload a JSON file to S3 bucket
+
+    Args:
+        local_file_path (str): The local object name/path
+        s3_object_name (str): The S3 object name/path
+        file_type (str): the file type
+    """
+    try:
+        # Upload the file
+        client.fput_object(
+            bucket_name,
+            s3_object_name,
+            local_file_path,
+            content_type=f"application/{file_type}",
+        )
+        print(f"Successfully uploaded {local_file_path} as {s3_object_name}")
+        return True
+    except Exception as e:
+        print(f"Error uploading {local_file_path}: {e}")
+        return False
+
+
 def list_bucket_objects():
     """List all objects in the bucket"""
     try:
@@ -84,6 +136,29 @@ def get_json_from_s3(s3_object_name, return_data=False):
         else:
             print(f"Successfully retrieved object {s3_object_name}")
             return response
+
+    except Exception as e:
+        print(f"Error getting {s3_object_name}: {e}")
+        return None
+
+
+def get_generic_file_from_s3(s3_object_name):
+    """
+    Get generic data from S3 bucket
+
+    Args:
+        s3_object_name (str): The S3 object name/path
+        return_data (bool): If True, return the data as Python object
+                           If False, return the raw response object
+
+    Returns:
+        response object
+    """
+    try:
+        # Get the object
+        response = client.get_object(bucket_name, s3_object_name)
+        print(f"Successfully retrieved object {s3_object_name}")
+        return response
 
     except Exception as e:
         print(f"Error getting {s3_object_name}: {e}")
